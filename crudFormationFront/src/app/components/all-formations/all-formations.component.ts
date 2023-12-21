45faab8093accc0b8fc1370885b0fe154dadaf71
mirfou <fourmitux111@gmail.com>
2023-12-21 12:12:10 +0100

    Premier contact avec le projet angular

---
+++ crudFormationFront/src/app/components/all-formations/all-formations.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-all-formations',
+  standalone: true,
+  imports: [],
+  templateUrl: './all-formations.component.html',
+  styleUrl: './all-formations.component.css'
+})
+export class AllFormationsComponent {
+
+}
